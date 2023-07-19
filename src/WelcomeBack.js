f7b00b2c3015b64564576e6ba510c89b8f2d15ba
Callie Giles <anchoredinthecrown@gmail.com>
2023-07-18 21:19:40 -0400

    add styling to welcomeBack.css

--- src/WelcomeBack.js
+++ src/WelcomeBack.js
@@ -1,7 +1,11 @@
 import "./WelcomeBack.css";
 
 function WelcomeBack({ name = "valued customer" }) {
-  return <p>Welcome back, {name}!</p>;
+  return (
+    <p>
+      Welcome back, <span className="username">{name}</span>!
+    </p>
+  );
 }
 
 export default WelcomeBack;
