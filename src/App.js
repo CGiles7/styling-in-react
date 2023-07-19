1ba0eb4103103151588978a1f53aa78ad706d7cc
Callie Giles <anchoredinthecrown@gmail.com>
2023-07-18 21:13:41 -0400

    starter code

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./Header";
+import WelcomeBack from "./WelcomeBack";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <WelcomeBack name="Joe" />
+      <WelcomeBack name="Anna" />
+      <WelcomeBack />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
