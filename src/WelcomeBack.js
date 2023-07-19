c863469b18e8ef92e48c2fcef1fbd927789fad42
Callie Giles <anchoredinthecrown@gmail.com>
2023-07-18 21:20:39 -0400

    add inline styling welcomeBack.js

--- src/WelcomeBack.js
+++ src/WelcomeBack.js
@@ -1,11 +1,21 @@
-import "./WelcomeBack.css";
-
 function WelcomeBack({ name = "valued customer" }) {
-  return (
-    <p>
-      Welcome back, <span className="username">{name}</span>!
-    </p>
-  );
-}
-
-export default WelcomeBack;
+    const pStyle = {
+      color: "black",
+      fontFamily: "sans-serif",
+    };
+    const userName = {
+      padding: "2px 4px",
+      marginRight: "2px",
+      backgroundColor: "#000",
+      color: "#fff",
+      borderRadius: "4px",
+    };
+  
+    return (
+      <p style={pStyle}>
+        Welcome back, <span style={userName}>{name}</span>!
+      </p>
+    );
+  }
+  
+  export default WelcomeBack;
